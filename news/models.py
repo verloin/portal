c09445319e202ed8e137dfb6022cf2620a5a92f8
yury matytsin <ura.assist@yandex.ru>
2022-04-19 19:08:39 +0300

    added img of author in newses

--- news/models.py
+++ news/models.py
@@ -11,6 +11,7 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    img_author = models.ImageField(default='user.jpg', upload_to='profile_images/')
     body = models.TextField(null=True, blank=True)
 
     class Meta:
