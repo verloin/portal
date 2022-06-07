2b882b69d82ff59bcec475e21c2d1374173790dd
yury matytsin <ura.assist@yandex.ru>
2022-06-07 14:55:54 +0300

    09-push

--- script_for_prepare_system_to_portal.sh
+++ script_for_prepare_system_to_portal.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-copy /var/www/portal/configs/gunicorn.socket /etc/systemd/system/
-copy /var/www/portal/configs/gunicorn.service /etc/systemd/system/
+cp /var/www/portal/configs/gunicorn.socket /etc/systemd/system/
+cp /var/www/portal/configs/gunicorn.service /etc/systemd/system/
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
 echo "GUNICORN sucessfully started"
-copy /var/www/portal/configs/portal /etc/nginx/sites-available/
+cp /var/www/portal/configs/portal /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/portal /etc/nginx/sites-enabled
 sudo nginx -t && sudo systemctl restart nginx
 echo "NGINX sucessfully started"
